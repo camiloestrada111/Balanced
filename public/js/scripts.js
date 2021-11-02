c7b5c2adefeec628d21be1024fe29fecc0ecd3de
camiloestrada111 <camiloestrada111@gmail.com>
2021-11-01 20:20:19 -0600

    commit

---
+++ public/js/scripts.js
@@ -0,0 +1,2 @@
+const heading = document.querySelector('.header__texto h2')
+console.log(heading)
---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(3000);
+console.log('Server on port 3000')
